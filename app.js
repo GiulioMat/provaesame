c9a7cd22d65bc637c4dd451d0203408b05e9df47
Stefano <stefano.pretto@unitn.it>
2019-01-14 11:33:17 +0100

    travis

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-
-
+const User = require('./models/user');
 
 app.use(express.static('./public'))
 
+var assignment = User.findOrCreate({
+    taskID: '1234',
+    assignmentID : 'nick', 
+    pasworkerID: 'nick',
+    assignmentResult: true 
+  });
+
+app.get("/", function(req,res){
+    res.send("Pippo");
+})
 
 app.listen(PORT, () => console.log('Example app listening on port: ' + PORT))
 
+// authentication routes
+var user = require('./routes/users');
+app.use('/users', user);
 
