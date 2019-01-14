3b0f6c215c1640dca8aeabeca5a3000bed55810d
Giulio <giulio.mattolin@studenti.unitn.it>
2019-01-14 10:41:54 +0100

    push

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+
+app.use(express.static('./public'))
+
+
+app.listen(PORT, () => console.log('Example app listening on port: ' + PORT))
+
 
